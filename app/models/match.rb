8ea5662bd17fa65fdb25270b39c2d1a6fda47d13
nhoxtien2010 <nhoxtien2010@gmail.com>
2018-03-05 21:35:04 +0700

    update relation for model

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,8 @@
 class Match < ApplicationRecord
+  belongs_to :group
+  has_one :match_result
+  belongs_to :venue
+  belongs_to :player, foreign_key: :player_1_id
+  belongs_to :player, foreign_key: :player_2_id
+
 end
