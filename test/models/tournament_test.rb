4178eab5c0e8e04abacb58669bb5083bca29b539
nhoxtien2010 <nhoxtien2010@gmail.com>
2018-03-07 00:46:30 +0700

    write test statistic for tournament

--- test/models/tournament_test.rb
+++ test/models/tournament_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class TournamentTest < ActiveSupport::TestCase
 
-  # test "the truth" do
-  #   assert true
-  # end
+  # test statistic of tournament
+  test 'test statistic of tournament' do
+    statistic = Tournament.first.statistic
+    assert statistic.size == 2
+    best_player = statistic.detect { |p| p['id'] == 2 }
+    assert best_player['total_points'] == 4
+    assert best_player['wins'] == 1
+    assert best_player['looses'].zero?
+    assert best_player['draws'] == 1
+    assert statistic.detect { |p| p['id'] == 1 }['total_points'] == 1
+  end
+
+
+
 end
