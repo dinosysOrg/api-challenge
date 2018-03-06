bc37f2790a8dcecc167ff824253cc9564c031583
nhoxtien2010 <nhoxtien2010@gmail.com>
2018-03-06 07:47:07 +0700

    update relation of match_result: gave_up_player_id

--- app/models/match_result.rb
+++ app/models/match_result.rb
@@ -1,5 +1,5 @@
 class MatchResult < ApplicationRecord
   belongs_to :match
   belongs_to :match_rule
-  belongs_to :player, foreign_key: :gave_up_player_id
+  belongs_to :player, foreign_key: :gave_up_player_id, optional: true
 end
