7015121e776798a82e263b42149cf1b543e46213
nhoxtien2010 <nhoxtien2010@gmail.com>
2018-03-07 00:01:59 +0700

    create tournament_test

--- test/models/tournament_test.rb
+++ test/models/tournament_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
-class GroupTest < ActiveSupport::TestCase
+class TournamentTest < ActiveSupport::TestCase
 
-  test 'should not save group without tournament' do
-    group = Group.new(name: 'test group')
-    assert_not group.save
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
